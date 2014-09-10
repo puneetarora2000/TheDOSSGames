387c5aee86e363f74c21b81e93bd74cea9c5258d
puneetarora2000 <puneetarora2000@gmail.com>
2014-09-10 12:10:03 +0530

    maths added

---
+++ src/evaluation/TestEvaluation.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package evaluation;
+
+import auc.AUCCalculator;
+import auc.ClassSort;
+import auc.Confusion;
+import auc.PNPoint;
+import auc.ReadList;
+
+
+
+/**
+ *
+ * @author Ecologic
+ */
+public class TestEvaluation {
+    
+    public static void main(String args[]){
+        
+        //java -jar auc.jar testset.txt PR 20 2000 0.5
+     //   AUCCalculator mm = new AUCCalculator();
+        
+        //ClassSort  mms = new ClassSort(d, i);
+        
+    //    Confusion cm = new Confusion(d, d1);
+      //  cm.calculateAUCROC();
+        
+        //PNPoint  mm = new PNPoint(d, d1)
+    
+        //ReadList  rl = new ReadList();
+        //rl
+
+      //  PerformanceMeasure pmSum = new PerformanceMeasure();
+        
+    
+    }
+}
---
+++ src/monitor/trace/TestTrace.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package monitor.trace;
+
+/**
+ *
+ * @author Ecologic
+ */
+public class TestTrace {
+ 
+    public static void main(String args[]){
+    
+        // Buid Trace :  
+    
+    
+    }
+    
+}
